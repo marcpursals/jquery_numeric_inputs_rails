45bbf53a3dc871fb1e1e66ae77c6087fdf89edc1
Marc Pursals <marc@marcpursals.com>
2014-09-07 12:10:49 +0200

    initial

---
+++ lib/jquery_numeric_inputs_rails.rb
@@ -0,0 +1,5 @@
+require "jquery_numeric_inputs_rails/version"
+
+module JqueryNumericInputsRails
+  # Your code goes here...
+end
---
+++ lib/jquery_numeric_inputs_rails/version.rb
@@ -0,0 +1,3 @@
+module JqueryNumericInputsRails
+  VERSION = "0.0.1"
+end
